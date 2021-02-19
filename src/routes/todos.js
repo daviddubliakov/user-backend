f19ad058c6dfbc966e3b881a8149aba2e568be5b
David Dubliakov <dubliakov1@gmail.com>
2021-02-19 17:39:57 +0200

    messages in cruds

--- src/routes/todos.js
+++ src/routes/todos.js
@@ -14,7 +14,7 @@ router.post('/create', async (req, res) => {
 
   newTodoObj.save(err => {
     if (err) return res.status(500).send(err);
-    return res.status(200).send(newTodoObj);
+    return res.status(200).send({ message: 'User successfully created!' });
   });
 })
 
@@ -22,13 +22,29 @@ router.post('/create', async (req, res) => {
 router.delete('/delete', (req, res) => {
   Todo.deleteOne(
     { _id: req.body._id }
-  ).then(() => res.status(200).send('User is successfully deleted!'));
+  ).then(() => res.status(200).send({ message: 'User is successfully deleted!' }));
 })
 
 router.get('/user/:_id', (req, res) => {
   Todo.findOne({ _id: req.params._id }, (err, user) => {
     res.send(user);
   });
+})
+
+router.put('/user/:_id', (req, res) => {
+  Todo.findOneAndUpdate({
+    _id: req.body._id
+  },
+    {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      age: req.body.age,
+      avatar: req.body.avatar,
+    })
+    .then(() => {
+      res.status(200).send({ message: 'User is successfully updated!' });
+    })
+    .catch(error => console.error(error))
 })
 
 export default router;
