c6093f26281195cde689d85c1256fb433df91742
David Dubliakov <dubliakov1@gmail.com>
2021-02-24 14:57:45 +0200

    cleared repo

--- src/models/User.js
+++ src/models/User.js
@@ -2,8 +2,8 @@ import { Schema, model } from 'mongoose';
 
 const userSchema = new Schema({
   firstName: { type: String, required: true },
-  age: { type: Number, required: true },
   lastName: { type: String, required: true },
+  age: { type: Number, required: true },
   avatar: String,
 });
 
